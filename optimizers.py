07825e0f5affff2a858409ef5f08c4a09f04b2bd
kyle-gao <67448832+kyle-gao@users.noreply.github.com>
2020-09-11 11:56:33 -0400

    Update optimizers.py

--- optimizers.py
+++ optimizers.py
@@ -1,3 +1,18 @@
+"""
+Copyright 2020 Yi Lin(Kyle) Gao
+#@title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License."""
+
+
 import numpy as np
 
 def sgd(Ws, Bs, dWs, dBs, vdw=0, vdb=0, sdw=0, sdb=0, lr=0.001, beta1=0.9, beta2=0.999, eps=1e-6, t=1):
